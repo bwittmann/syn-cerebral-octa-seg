4d2757cd7ff169146f1fde7b711fa12f8830e74d
Bastian Wittmann <bastian.wittmann@uzh.ch>
2024-04-16 16:56:11 +0200

    Updated inference script.

--- syn_cerebral_octa_seg/inference.py
+++ syn_cerebral_octa_seg/inference.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import torch
 import torch.nn.functional as F
+import numpy as np
 from scipy.ndimage import median_filter
 from tqdm import tqdm
 from torch.cuda.amp import autocast
@@ -17,6 +18,15 @@ from syn_cerebral_octa_seg.models import get_model
 
 def inference(args):
     path_to_run = Path('./runs/' + args.run)
+
+    args_modifier = args.run    # for logging purposes
+    if args.last:
+        args_modifier += '_last'
+    if args.ind_per:
+        args_modifier += '_indper'
+    if args.ensemble:
+        args_modifier += '_ensemble'
+
     device = torch.device('cpu') if args.cpu else torch.device('cuda:' + str(args.num_gpu))
     config = read_json(path_to_run / 'config.json')
 
@@ -41,45 +51,49 @@ def inference(args):
     model.eval()
 
     # load raw data
-    files = [file for file in Path(args.data_folder).glob('**/*') if file.is_file() and file.suffix in ['.tif', '.nii']]
+    files = [file for file in Path(args.data_folder).glob('**/*') if file.is_file() and file.suffix in ['.tif']]
 
     # perform inference
-    for file in tqdm(files):
-        print(f'Processing {file}.')
-
-        try:
-            data = read_tiff(file)
-        except:
-            data = read_nifti(file)
+    for file in files:
+        print(f'Processing {file}')
+        data = read_tiff(file).astype(np.float32)
 
         # transform data
-        data = preprocess_image(data)[None][None]
+        data = preprocess_image(data, ind_percentile=args.ind_per)[None][None].to(device=device)
 
         # make prediction
-        with autocast():
-            out = sliding_window_inference(
-                inputs=data,
-                roi_size=config['augmentation']['patch_size'],
-                sw_batch_size=config['batch_size'],
-                predictor=model,
-                overlap=args.overlap,
-                mode=config['sliding_window_mode'],
-                padding_mode='constant',
-                inference=True
-            )
+        with torch.no_grad():
+            with autocast():
+                out = sliding_window_inference(
+                    inputs=data,
+                    roi_size=config['augmentation']['patch_size'],
+                    sw_batch_size=config['batch_size'],
+                    predictor=model,
+                    overlap=args.overlap,
+                    mode=config['sliding_window_mode'],
+                    padding_mode='constant',
+                    progress=True,
+                    inference=True
+                )
 
         # save segmentation masks
-        write_nifti((out >= args.threshold).int().squeeze().cpu().numpy(), args.data_folder / ('seg_' + Path(args.inference).parts[-1] + '.nii'))
-        print(f'Finished processing {file}.')
+        write_nifti((out >= args.threshold).int().squeeze().cpu().numpy(), args.data_folder / Path(file.parts[-1].split('.')[0] + '_' + args_modifier + '_seg.nii'))
+        write_nifti(data.squeeze().cpu().numpy(), args.data_folder / Path(file.parts[-1].split('.')[0] + '_' + args_modifier + '_data.nii'))
+
+        print(f'Finished processing {file}.', '\n')
 
 def preprocess_image(
         img, scale_factor=1.53, percentile_lower_pre=2.0915021896362305, percentile_upper_pre=878.1568603515625, 
-        percentile_lower_post=0.00019024491484742612, percentile_upper_post=1.0
+        percentile_lower_post=0.00019024491484742612, percentile_upper_post=1.0, ind_percentile=False
     ):
-    # isotropic voxel size
-    interpolated_img = F.interpolate(img[None][None].float(), scale_factor=(scale_factor, 1, 1), mode='trilinear').squeeze()
+    # isotropic voxel size (2um)
+    interpolated_img = F.interpolate(torch.tensor(img)[None][None], scale_factor=(scale_factor, 1, 1), mode='trilinear').squeeze()
 
-    # pre scale and clip 
+    # pre scale and clip
+    if ind_percentile:
+        percentile_lower_pre = np.percentile(interpolated_img, 1)
+        percentile_upper_pre = np.percentile(interpolated_img, 97)
+
     transform_pre = ScaleIntensityRange(percentile_lower_pre, percentile_upper_pre, 0, 1, clip=True)    # percent [1, 97] of m4 raw (angioTestSet_fullReference_raw_unscaled_m4.tif)
     interpolated_img_transformed = transform_pre(interpolated_img)
 
@@ -87,6 +101,10 @@ def preprocess_image(
     interpolated_img_transformed = median_filter(interpolated_img_transformed, size=3)
         
     # post scale and clip
+    if ind_percentile:
+        percentile_lower_post = np.percentile(interpolated_img, 1)
+        percentile_upper_post = np.percentile(interpolated_img, 99)
+
     transform_post = ScaleIntensityRange(percentile_lower_post, percentile_upper_post, 0, 1, clip=True) # percent [1, 99] of m4 raw (angioTestSet_fullReference_raw_unscaled_m4.tif)
     interpolated_img_transformed = transform_post(interpolated_img_transformed)
 
@@ -101,9 +119,11 @@ if __name__ == "__main__":
     parser.add_argument('--run', required=True, type=str, help='Name of experiment in syn_cerebral_octa_seg/runs.')
     parser.add_argument('--num_gpu', type=int, default=0, help='Id of GPU to run inference on.')
     parser.add_argument('--threshold', type=float, default=0.5, help='Threshold for foreground voxels.')
-    parser.add_argument('--overlap', type=float, default=0.5, help='Overlap in sliding window inference scheme.')
+    parser.add_argument('--overlap', type=float, default=0.8, help='Overlap in sliding window inference scheme.')
     parser.add_argument('--last', action='store_true', help='Use model_last instead of model_best.')
     parser.add_argument('--cpu', action='store_true', help='Utilize cpu.')
+    parser.add_argument('--ind_per', action='store_true', help='Estimate for each image an individual percentile value.')
+    parser.add_argument('--ensemble', action='store_true', help='Use 6-fold ensemble for inference.')
     args = parser.parse_args()
 
     # run inference
